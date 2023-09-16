8baf256e2b956dbee65537883729fa70bf0c497c
shnaboi <shane.karner@gmail.com>
2023-09-16 15:14:02 -0400

    Day 3 exercise, Leap Year exercise

---
+++ day03/Leap_Year_Exercise.py
@@ -0,0 +1,11 @@
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0 and year % 100 == 0:
+    if year % 400 == 0:
+        print("Leap year.")
+    else:
+        print("Not a leap year.")
+elif year % 4 == 0:
+    print("Leap year.")
+else:
+    print("Not a leap year.")
