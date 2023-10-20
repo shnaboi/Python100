576d0db85d473c5a681e4d2b1d0bfae2c1c3b4f6
Shane Karner <shane.karner@gmail.com>
2023-10-19 23:06:09 -0400

    Complete Day16, rebuild coffee machine with objects / OOP

---
+++ Day16/oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -0,0 +1,24 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+  options = menu.get_items()
+  choice = input(f"What would you like to order? {options}")
+  if choice == "off":
+    is_on = False
+  elif choice == "report":
+    money_machine.report()
+    coffee_maker.report()
+  else:
+    drink = menu.find_drink(choice)
+    if coffee_maker.is_resource_sufficient(drink):
+      if money_machine.make_payment(drink.cost):
+        coffee_maker.make_coffee(drink)
+
