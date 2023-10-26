fe1fe4af65878f578131b7f0378fb5fb7c59b808
Shane Karner <shane.karner@gmail.com>
2023-10-26 00:47:23 -0400

    Reformat code and adding basic knowledge quiz data

--- Day17/QuizGame/main.py
+++ Day17/QuizGame/main.py
@@ -1,10 +1,10 @@
 from question_model import Question
-from data import question_data, question_data_2
+from data import question_data, question_data_2, question_data_3
 from quiz_brain import QuizBrain
 
 question_bank = []
 
-for q in question_data_2:
+for q in question_data_3:
   var = Question(q["question"], q['correct_answer'])
   question_bank.append(var)
 
