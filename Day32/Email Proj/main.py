8e5a679045d6e44f5396a3f9928942a7d737155b
Shane Karner <shane.karner@gmail.com>
2023-12-29 12:14:37 -0500

    Refactor code and add comments to explain the hard stuff

--- Day32/Email Proj/main.py
+++ Day32/Email Proj/main.py
@@ -1,3 +1,6 @@
+# Check out Day 32 solution for:
+# dict comprehension, pandas library, data frames, file manipulation
+
 import random
 import smtplib
 # Documentation: https://docs.python.org/3/library/smtplib.html
@@ -15,6 +18,9 @@ today = (month, day)
 
 df = pandas.read_csv('./birthdays.csv')
 today_bdays = df[(df["month"] == month) & (df['day'] == day)]
+# today_bdays is a dataframe containing entire rows of
+# name, email, and date(yr, mo, day) that match today.
+# We convert it to dict in the else statement below
 
 if today_bdays.empty:
   pass
@@ -24,12 +30,9 @@ else:
     name = bday["name"]
     email = bday["email"]
     letter_num = random.randint(1, 3)
-    print(letter_num)
     with open(f"./letter_templates/letter_{letter_num}.txt", mode="r") as letter:
-      message = letter.readlines()
-      msg = ''.join(message)
-      named_message = msg.replace("[NAME]", name)
-      print(named_message)
+      msg = letter.read()
+      named_msg = msg.replace("[NAME]", name)
       with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
@@ -37,5 +40,5 @@ else:
           from_addr=my_email,
           to_addrs=email,
           msg=f"Subject:Happy Birthday\n\n"
-              f"{named_message}"
+              f"{named_msg}"
         )
