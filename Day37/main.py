fd95727be2c13a0035ff7ebde4c16103f1c0075c
Shane Karner <shane.karner@gmail.com>
2024-01-15 14:22:50 -0500

    Refactor Code

--- Day37/main.py
+++ Day37/main.py
@@ -2,15 +2,21 @@ import requests
 import os
 import datetime as dt
 
+# PIXELA DOCUMENTATION
+# https://docs.pixe.la/
+
 USERNAME = "shnaboi"
 TOKEN = os.environ.get("PIXELA_TOKEN")
 HEADERS = {
   "X-USER-TOKEN": TOKEN
 }
+graph_1 = "test1"
+PIXELA_ENDPOINT = "https://pixe.la/v1/users/shnaboi"
+
 now = dt.datetime.now()
 date = now.strftime("%Y%m%d")
 
-graph_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+graph_endpoint = f"{PIXELA_ENDPOINT}/graphs"
 graph_body_req = {
   "id": "test1",
   "name": "test1 Graph",
@@ -22,7 +28,7 @@ graph_body_req = {
 # response = requests.post(url=graph_endpoint, json=graph_body_req, headers=HEADERS)
 # print(response.text)
 
-post_pixel_endpoint = "https://pixe.la/v1/users/shnaboi/graphs/test1"
+post_pixel_endpoint = f"{PIXELA_ENDPOINT}/graphs/test1"
 post_pixel_body = {
   "date": "20240114",
   "quantity": "1",
@@ -31,7 +37,7 @@ post_pixel_body = {
 # response = requests.post(url=post_pixel_endpoint, json=post_pixel_body, headers=HEADERS)
 # print(response.text)
 
-update_pixel_endpoint = "https://pixe.la/v1/users/shnaboi/graphs/test1/20240115"
+update_pixel_endpoint = f"{PIXELA_ENDPOINT}/graphs/test1/20240115"
 update_pixel = {
   "quantity": "0"
 }
