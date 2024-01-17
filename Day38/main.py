3029300c823fbd1ce08f6ea31d2794cf6e3e7921
Shane Karner <shane.karner@gmail.com>
2024-01-17 01:56:01 -0500

    Sheety API post method works with nutritionix API json data

--- Day38/main.py
+++ Day38/main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import datetime as dt
 
 APP_ID = os.environ.get("NUTRITIONIX_APP_ID")
 API_KEY = os.environ.get("NUTRITIONIX_API_KEY")
@@ -7,8 +8,12 @@ HEADER = {
   "x-app-id": APP_ID,
   "x-app-key": API_KEY
 }
-
-print(HEADER)
+now = dt.datetime.now()
+today = now.strftime("%m/%d/%Y")
+now_time = now.strftime("%H:%M:%S")
+# print(today)
+#
+# print(HEADER)
 
 NUTRITIONIX_ENDPOINT = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
@@ -19,4 +24,23 @@ exercise_params = {
 
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=exercise_params, headers=HEADER)
 response.raise_for_status()
-print(response.json())
+# print(response.json())
+nutritionix_data = response.json()
+
+sheety_endpoint = "https://api.sheety.co/82235a205924c90499f09a2066926078/myWorkoutsApp/workouts"
+
+sheety_data = {
+  "workout": {
+    "date": today,
+    "time": now_time,
+    "exercise": nutritionix_data['exercises'][0]['name'],
+    "duration": nutritionix_data['exercises'][0]['duration_min'],
+    "calories": nutritionix_data['exercises'][0]['nf_calories']
+  }
+}
+
+print(sheety_data)
+
+response_b = requests.post(url=sheety_endpoint, json=sheety_data)
+response_b.raise_for_status()
+print(response_b.text)
