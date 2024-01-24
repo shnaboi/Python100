705d056afb77fd60b6982a3d09e4bad9027a54dd
Shane Karner <shane.karner@gmail.com>
2024-01-23 23:24:51 -0500

    Tequila API to search and find city IATA codes works

--- Day39/flight_search.py
+++ Day39/flight_search.py
@@ -1,8 +1,40 @@
+# This class is responsible for searching for iata codes and flight prices
+import os
 import requests
-SHEETY_ENDPOINT = "https://api.sheety.co/82235a205924c90499f09a2066926078/flightDealsApp/flights"
+
+TEQUILA_API_KEY = os.environ.get("TEQUILA_API_KEY")
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
+TEQUILA_ID = "shnaboishnaflightprices"
+TEQUILA_HEADER = {
+  "apikey": TEQUILA_API_KEY
+}
 class FlightSearch:
   def __init__(self):
     pass
   def get_iata_code(self, city):
-    code = "TESTING_2"
+    params = {
+      "term": city
+    }
+    response = requests.get(url="https://api.tequila.kiwi.com/locations/query", params=params, headers=TEQUILA_HEADER)
+    response.raise_for_status()
+    data = response.json()
+    code = data["locations"][0]["code"]
     return code
+
+tequila_params = {
+  "fly_from": "(city code)",
+  "fly_to": "(multiple city codes)",
+  "date_from": "DD/MM/YYY",
+  "date_to": "DD/MM/YYYY",
+  "limit": 15
+}
+
+params = {
+  "fly_from": "CVG",
+  "date_from": "02/04/2024",
+  "date_to": "03/04/2024",
+  "limit": 2
+}
+t_response = requests.get(url=TEQUILA_ENDPOINT, params=params, headers=TEQUILA_HEADER)
+t_response.raise_for_status()
+print(t_response.text)
--- Day39/main.py
+++ Day39/main.py
@@ -16,99 +16,74 @@ We're also looking for round trips that return between 7 and 28 days in length.
 The currency of the price we get back should be in USD.
 """
 
-# sheety_data = {
-#   "flights": [
-#     {
-#       "city": "Paris",
-#       "iataCode": "",
-#       "lowestPrice": 54,
-#       "id": 2
-#     },
-#     {
-#       "city": "Berlin",
-#       "iataCode": "",
-#       "lowestPrice": 42,
-#       "id": 3
-#     },
-#     {
-#       "city": "Tokyo",
-#       "iataCode": "",
-#       "lowestPrice": 485,
-#       "id": 4
-#     },
-#     {
-#       "city": "Sydney",
-#       "iataCode": "",
-#       "lowestPrice": 551,
-#       "id": 5
-#     },
-#     {
-#       "city": "Istanbul",
-#       "iataCode": "",
-#       "lowestPrice": 95,
-#       "id": 6
-#     },
-#     {
-#       "city": "Kuala Lumpur",
-#       "iataCode": "",
-#       "lowestPrice": 414,
-#       "id": 7
-#     },
-#     {
-#       "city": "New York",
-#       "iataCode": "",
-#       "lowestPrice": 240,
-#       "id": 8
-#     },
-#     {
-#       "city": "San Francisco",
-#       "iataCode": "",
-#       "lowestPrice": 260,
-#       "id": 9
-#     },
-#     {
-#       "city": "Cape Town",
-#       "iataCode": "",
-#       "lowestPrice": 378,
-#       "id": 10
-#     }
-#   ]
-# }
+sheety_data = [
+    {
+      "city": "Paris",
+      "iataCode": "",
+      "lowestPrice": 54,
+      "id": 2
+    },
+    {
+      "city": "Berlin",
+      "iataCode": "",
+      "lowestPrice": 42,
+      "id": 3
+    },
+    {
+      "city": "Tokyo",
+      "iataCode": "",
+      "lowestPrice": 485,
+      "id": 4
+    },
+    {
+      "city": "Sydney",
+      "iataCode": "",
+      "lowestPrice": 551,
+      "id": 5
+    },
+    {
+      "city": "Istanbul",
+      "iataCode": "",
+      "lowestPrice": 95,
+      "id": 6
+    },
+    {
+      "city": "Kuala Lumpur",
+      "iataCode": "",
+      "lowestPrice": 414,
+      "id": 7
+    },
+    {
+      "city": "New York",
+      "iataCode": "",
+      "lowestPrice": 240,
+      "id": 8
+    },
+    {
+      "city": "San Francisco",
+      "iataCode": "",
+      "lowestPrice": 260,
+      "id": 9
+    },
+    {
+      "city": "Cape Town",
+      "iataCode": "",
+      "lowestPrice": 378,
+      "id": 10
+    }
+]
 
-data_man_obj = DataManager()
-sheety_data = data_man_obj.get_travel_data()
+# data_man_obj = DataManager()
+# sheety_data = data_man_obj.get_travel_data()
 # ^^^ get sheety data
 print(f"sheety_data = {sheety_data}")
 
 flight_search_obj = FlightSearch()
 for flight in sheety_data:
-  if flight["iataCode"] == "testing":
+  if flight["iataCode"] == "":
     flight["iataCode"] = flight_search_obj.get_iata_code(flight["city"])
 
-data_man_obj.travel_data = sheety_data
-data_man_obj.update_iata_code()
+# data_man_obj.travel_data = sheety_data
+# data_man_obj.update_iata_code()
 
-# TEQUILA_API_KEY = os.environ.get("TEQUILA_API_KEY")
-# TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/v2/search"
-# TEQUILA_ID = "shnaboishnaflightprices"
-# TEQUILA_HEADER = {
-#   "apikey": TEQUILA_API_KEY
-# }
-#
-# tequila_params = {
-#   "fly_from": "(city code)",
-#   "fly_to": "(multiple city codes)",
-#   "date_from": "DD/MM/YYY",
-#   "date_to": "DD/MM/YYYY",
-#   "limit": 15
-# }
-#
-# params = {
-#   "fly_from": "FRA",
-#   "date_from": "02/04/2024",
-#   "date_to": "03/04/2024",
-#   "limit": 2
-# }
-# t_response = requests.get(url=TEQUILA_ENDPOINT, params=params, headers=TEQUILA_HEADER)
-# t_response.raise_for_status()
-# print(t_response.text)
+print(sheety_data)
