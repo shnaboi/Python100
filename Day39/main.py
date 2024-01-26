a6b6a199c0c9d6d66ff34006679ae4146e0e5ce9
Shane Karner <shane.karner@gmail.com>
2024-01-25 23:53:43 -0500

    Running code and refactor/commenting in sheety code to update sheety and run script <-- it works

--- Day39/main.py
+++ Day39/main.py
@@ -4,7 +4,6 @@
 import requests
 import os
 import datetime as dt
-from pprint import pprint
 from flight_search import FlightSearch
 from data_manager import DataManager
 
@@ -16,67 +15,67 @@ We're also looking for round trips that return between 7 and 28 days in length.
 The currency of the price we get back should be in USD.
 """
 
-sheety_data = [
-    {
-      "city": "Paris",
-      "iataCode": "",
-      "lowestPrice": 54,
-      "id": 2
-    },
-    {
-      "city": "Berlin",
-      "iataCode": "",
-      "lowestPrice": 42,
-      "id": 3
-    },
-    {
-      "city": "Tokyo",
-      "iataCode": "",
-      "lowestPrice": 485,
-      "id": 4
-    },
-    {
-      "city": "Sydney",
-      "iataCode": "",
-      "lowestPrice": 551,
-      "id": 5
-    },
-    {
-      "city": "Istanbul",
-      "iataCode": "",
-      "lowestPrice": 95,
-      "id": 6
-    },
-    {
-      "city": "Kuala Lumpur",
-      "iataCode": "",
-      "lowestPrice": 414,
-      "id": 7
-    },
-    {
-      "city": "New York",
-      "iataCode": "",
-      "lowestPrice": 240,
-      "id": 8
-    },
-    {
-      "city": "San Francisco",
-      "iataCode": "",
-      "lowestPrice": 260,
-      "id": 9
-    },
-    {
-      "city": "Cape Town",
-      "iataCode": "",
-      "lowestPrice": 378,
-      "id": 10
-    }
-]
+# sheety_data = [
+#     {
+#       "city": "Paris",
+#       "iataCode": "",
+#       "lowestPrice": 54,
+#       "id": 2
+#     },
+#     {
+#       "city": "Berlin",
+#       "iataCode": "",
+#       "lowestPrice": 42,
+#       "id": 3
+#     },
+#     {
+#       "city": "Tokyo",
+#       "iataCode": "",
+#       "lowestPrice": 485,
+#       "id": 4
+#     },
+#     {
+#       "city": "Sydney",
+#       "iataCode": "",
+#       "lowestPrice": 551,
+#       "id": 5
+#     },
+#     {
+#       "city": "Istanbul",
+#       "iataCode": "",
+#       "lowestPrice": 95,
+#       "id": 6
+#     },
+#     {
+#       "city": "Kuala Lumpur",
+#       "iataCode": "",
+#       "lowestPrice": 414,
+#       "id": 7
+#     },
+#     {
+#       "city": "New York",
+#       "iataCode": "",
+#       "lowestPrice": 240,
+#       "id": 8
+#     },
+#     {
+#       "city": "San Francisco",
+#       "iataCode": "",
+#       "lowestPrice": 260,
+#       "id": 9
+#     },
+#     {
+#       "city": "Cape Town",
+#       "iataCode": "",
+#       "lowestPrice": 378,
+#       "id": 10
+#     }
+# ]
 
-# data_man_obj = DataManager()
-# sheety_data = data_man_obj.get_travel_data()
+data_man_obj = DataManager()
+sheety_data = data_man_obj.get_travel_data()
 # ^^^ get sheety data
-# print(f"sheety_data = {sheety_data}")
+print(f"sheety_data = {sheety_data}")
 
 # Get IATA Code using FlightSearch()
 flight_search_obj = FlightSearch()
@@ -85,8 +84,8 @@ for flight in sheety_data:
     flight["iataCode"] = flight_search_obj.get_iata_code(flight["city"])
 
 # Update Sheety
-# data_man_obj.travel_data = sheety_data
-# data_man_obj.update_iata_code()
+data_man_obj.travel_data = sheety_data
+data_man_obj.update_iata_code()
 
 # print(sheety_data)
 
@@ -98,4 +97,3 @@ for flight in sheety_data:
   flight_data_obj.organize_data(data_from_search)
 
 print(flight_data_obj.paris)
-
