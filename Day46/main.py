231d6a8b4130e8a9958a5071a3f18e0a1d7cb907
Shane Karner <shane.karner@gmail.com>
2024-02-07 23:59:09 -0500

    Correctly communicating with spotify api, was able to generate a playlist on shnaboi spotify account

--- Day46/main.py
+++ Day46/main.py
@@ -1,5 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Set up OAuth handler
+sp_oauth = SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                        client_secret=SPOTIFY_CLIENT_SECRET,
+                        redirect_uri=SPOTIFY_REDIRECT_URI,
+                        scope='playlist-modify-private playlist-modify-public')
+
+# Get the token
+token_info = sp_oauth.get_access_token(as_dict=False)
+
+# Create a Spotipy instance using the obtained token
+sp = spotipy.Spotify(auth=token_info)
+
+# Example request: Fetch the current user's playlists
+playlists = sp.current_user_playlists()
+
+playlist_name = 'Generated Playlist'
+
+playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
+
+playlist_id = playlist['id']
+
+print(playlist_id, f"token = {token_info}")
 
 response = requests.get("https://www.billboard.com/charts/hot-100/2001-05-10/")
 website = response.text
@@ -12,7 +37,6 @@ soup = BeautifulSoup(website, "html.parser")
 
 song_names = soup.select("div div ul li ul li h3")
 artist_names = soup.find_all(name="span", class_="a-no-trucate")
-print(len(song_names), len(artist_names))
 
 top_100_dict = {}
 for x in range(len(song_names)):
