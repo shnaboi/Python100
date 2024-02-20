7816f111760b10cbebe14ea60c691257565bf139
Shane Karner <shane.karner@gmail.com>
2024-02-20 11:31:03 -0500

    Remove beautifulsoup4 syntax, using Selenium only

--- Day47/main.py
+++ Day47/main.py
@@ -1,5 +1,3 @@
-from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,11 +5,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
 amazon_product = "https://www.amazon.com/Yonex-EZONE-Night-Tennis-Racquet/dp/B0CS8DQGT3/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_w=GpDMq&content-id=amzn1.sym.225b4624-972d-4629-9040-f1bf9923dd95%3Aamzn1.symc.40e6a10e-cbc4-4fa5-81e3-4435ff64d03b&pf_rd_p=225b4624-972d-4629-9040-f1bf9923dd95&pf_rd_r=9MC9ZZW3Q566XV9GMD3M&pd_rd_wg=DWAor&pd_rd_r=7725b019-8907-4ca2-a152-f68bde23231d&pd_rd_i=B0CS8DCPRQ&th=1"
-
-header = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-  "Accept-Language": "en-US,en;q=0.9"
-}
 
 # Create ChromeOptions object
 chrome_options = Options()
@@ -29,19 +22,12 @@ driver.get(amazon_product)
 print("line 28")
 
 try:
-    price_element = driver.find_element(By.CLASS_NAME, "a-price-whole")
+  price_element = driver.find_element(By.CLASS_NAME, "a-price-whole")
 
-    print("try succeeded")
-    # Extract the price text
-    price = price_element.text
-    print('Price:', price)
+  print("try succeeded")
+  # Extract the price text
+  price = price_element.text
+  print('Price:', price)
 finally:
-    # Close the WebDriver
-    driver.quit()
-
-
-# response = requests.get(amazon_product, headers=header)
-# webpage = response.text
-# soup = BeautifulSoup(webpage, "lxml")
-#
-# print(soup)
+  # Close the WebDriver
+  driver.quit()
