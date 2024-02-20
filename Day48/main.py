bc3cbb70672656b555176409e98bfffe37caa51a
Shane Karner <shane.karner@gmail.com>
2024-02-20 12:25:58 -0500

    Get list of ul elements and loop through each, complete challenge 1

--- Day48/main.py
+++ Day48/main.py
@@ -6,7 +6,13 @@ website = "https://www.python.org/"
 driver = webdriver.Chrome()
 
 driver.get(website)
-events = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/h2')
+events = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[2]/div[2]/div/ul')
 
-print(events.text)
+li_items = events.find_elements(By.TAG_NAME, "li")
+num = 1
+for li in li_items:
+  date_and_title = li.text
+  print(f"{num}: {date_and_title}")
+  num += 1
+
 
