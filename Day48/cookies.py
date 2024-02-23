dc23b0c0e61d6a598a285a5383bb2485de0b848e
Shane Karner <shane.karner@gmail.com>
2024-02-23 12:37:13 -0500

    Adjust algorithm to make efficient upgrades based on cps, money and upgrade costs

--- Day48/cookies.py
+++ Day48/cookies.py
@@ -9,40 +9,80 @@ driver = webdriver.Chrome()
 driver.get(website)
 driver.maximize_window()
 
-time.sleep(3)
+time.sleep(2)
 
 cookie = driver.find_element(By.XPATH, '//*[@id="cookie"]')
 
 game_on = True
+start_time = time.time()
+timeout = 5
+
 def play_game():
-  timeout = time.time() + 3
+  timeout = time.time() + 5
+  num = 0
   while game_on:
     cookie.click()
+    num += 1
     if time.time() > timeout:
       break
   check_upgrades()
 
 def check_upgrades():
-  # get upgrade elements
+  # get html elements
   upg_cursor = driver.find_element(By.ID, 'buyCursor')
   upg_granny = driver.find_element(By.ID, 'buyGrandma')
   upg_factory = driver.find_element(By.ID, 'buyFactory')
   upg_mine = driver.find_element(By.ID, 'buyMine')
   upg_shipment = driver.find_element(By.ID, 'buyShipment')
 
-  # get upgrade elements class
-  granny_val = upg_granny.get_attribute("class")
-  factory_val = upg_factory.get_attribute("class")
-  mine_val = upg_mine.get_attribute("class")
-  shipment_val = upg_shipment.get_attribute("class")
+  # get html elements class
+  granny_class = upg_granny.get_attribute("class")
+  factory_class = upg_factory.get_attribute("class")
+  mine_class = upg_mine.get_attribute("class")
+  shipment_class = upg_shipment.get_attribute("class")
 
-  if not shipment_val:
+  cps = driver.find_element(By.ID, 'cps').text
+  cps_string = ''.join(num for num in cps if num.isdigit())
+  cps_num = float(cps_string)
+
+  money = float((driver.find_element(By.ID, 'money').text).replace(",", ""))
+
+  b_cursor = upg_cursor.find_element(By.TAG_NAME, "b").text
+  cursor_cost = float(''.join(num for num in b_cursor if num.isdigit()))
+  b_granny = upg_granny.find_element(By.TAG_NAME, "b").text
+  granny_cost = float(''.join(num for num in b_granny if num.isdigit()))
+  b_factory = upg_factory.find_element(By.TAG_NAME, "b").text
+  factory_cost = float(''.join(num for num in b_factory if num.isdigit()))
+  b_mine = upg_mine.find_element(By.TAG_NAME, "b").text
+  mine_cost = float(''.join(num for num in b_mine if num.isdigit()))
+  b_shipment = upg_shipment.find_element(By.TAG_NAME, "b").text
+  shipment_cost = float(''.join(num for num in b_shipment if num.isdigit()))
+
+  if (cps_num + money + 300) >= shipment_cost:
+    upg_time = time.time() + 5
+    while time.time() < upg_time:
+      cookie.click()
     upg_shipment.click()
-  elif not mine_val:
+    print('ship worth it')
+  elif (cps_num + money + 150) >= mine_cost:
+    upg_time = time.time() + 3
+    while time.time() < upg_time:
+      cookie.click()
     upg_mine.click()
-  elif not factory_val:
+    print('mine worth it')
+  elif (cps_num + money + 55) >= factory_cost:
+    upg_time = time.time() + 1
+    while time.time() < upg_time:
+      cookie.click()
     upg_factory.click()
-  elif not granny_val:
+    print("factory worth it")
+  elif not shipment_class:
+    upg_shipment.click()
+  elif not mine_class:
+    upg_mine.click()
+  elif not factory_class:
+    upg_factory.click()
+  elif not granny_class:
     upg_granny.click()
   else:
     upg_cursor.click()
