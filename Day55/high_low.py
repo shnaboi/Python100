8649be7b201e8b9c61b9f656d6d379836891eb29
Shane Karner <shane.karner@gmail.com>
2024-03-07 14:08:18 -0500

    Finish Day55 game

--- Day55/high_low.py
+++ Day55/high_low.py
@@ -3,7 +3,10 @@ import random
 
 app = Flask(__name__)
 
-gif = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXc5d3hna3FxNDFzcDZrZG00NHhvdTBpbXM2MGpjaG8ybmRldG00MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LfbDxyQIWtzLTtMnc0/giphy.gif"
+gif_start = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXc5d3hna3FxNDFzcDZrZG00NHhvdTBpbXM2MGpjaG8ybmRldG00MyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LfbDxyQIWtzLTtMnc0/giphy.gif"
+gif_high = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDhvYmp4dW14OXhoeXl2ajRuMWYxOHV6ODl4OTMxbW5wM244NXB1NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3s95JDGDKf24tfDDXv/giphy.gif"
+gif_low = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWdnazFhZHk5bzNtNzNndXJrbWloa3hzY2tkcDI4OHI3bzl0bHQ3YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7bu56G2vRwulV3fa/giphy.gif"
+gif_correct = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTUzMHkzd3k2NHBmYXlieHRxMW5oeTE0YzBnY2I0MDR3cGszanZ1MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fah08IDMr10VtDrcoh/giphy.gif"
 
 def detect_route(func):
   def wrapper():
@@ -23,7 +26,7 @@ num_string = f"/{num}"
 def homepage():
   html = f"""
   <h1>Higher / Lower Guessing Game<h1>
-  <img src={gif}>
+  <img src={gif_start}>
   <p>Change the number in the address bar to guess a number<p>
   """
   return html
@@ -35,14 +38,19 @@ def num_guess(guess):
   if int(guess) == num:
     html = f"""
       <h1>You guessed the correct answer! Reload the server to play again.</h1>
+      <img src={gif_correct}>
       """
   elif num < int(guess):
     html = f"""
       <h1>You're guess is too <strong>high</strong>.</h1>
+      <img src={gif_high}>
+      <p>Drink some water.</p>
       """
   else:
     html = f"""
-      <h1>You're guess is too <strong>low</strong>.
+      <h1>You're guess is too <strong>low</strong>.</h1>
+      <img src={gif_low}>
+      <p>You're cursed forever! Get haunted!</p>
       """
   return html
 
