5e0f01ecb38a1f1dd553a8f0f5456dcbe4e033cd
Shane Karner <shane.karner@gmail.com>
2024-03-12 16:03:04 -0400

    Add api to server to estimate age & gender of user inputed name

--- Day57/main.py
+++ Day57/main.py
@@ -7,6 +7,16 @@ import datetime as dt
 genderize_API = "https://api.genderize.io?"
 agify_API = "https://api.agify.io?"
 
+# gender_response = requests.get(url=f"{genderize_API}name={}")
+# gender_response.raise_for_status()
+# shane1 = gender_response.json()
+#
+# age_response = requests.get(url=f"{agify_API}name={}")
+# age_response.raise_for_status()
+# shane2 = age_response.json()
+
+# print(shane1, shane2)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,6 +25,22 @@ def home():
   year = dt.datetime.now().year
   year_str = str(year)
   return render_template("server.html", num=random_num, year=year_str)
+
+@app.route("/<name>")
+def name_results(name):
+  gender_response = requests.get(url=f"{genderize_API}name={name}")
+  gender_response.raise_for_status()
+  gender_json = gender_response.json()
+  age_response = requests.get(url=f"{agify_API}name={name}")
+  age_response.raise_for_status()
+  age_json = age_response.json()
+  random_num = random.randint(69, 420)
+
+  return render_template("server.html",
+                         num=random_num,
+                         name_gender=gender_json['gender'],
+                         name_age=age_json['age'],
+                         name=name)
 
 if __name__ == "__main__":
   app.run()
