e5e7e03d254e672f6dd818a3a1831d0816a7c7eb
Shane Karner <shane.karner@gmail.com>
2024-03-13 13:26:24 -0400

    finish Day57

--- Day57/main.py
+++ Day57/main.py
@@ -42,6 +42,13 @@ def name_results(name):
                          name_age=age_json['age'],
                          name=name)
 
+@app.route("/blog")
+def blog_posts():
+  response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+  response.raise_for_status()
+  blog_data = response.json()
+  return render_template("blog.html", blogs=blog_data)
+
 if __name__ == "__main__":
   app.run()
 
