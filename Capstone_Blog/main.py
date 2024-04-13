6a36e673416368de6baf9a89fd34babf3ff3d495
Shane Karner <shane.karner@gmail.com>
2024-04-12 23:59:01 -0400

    Add Start Bootstrap template html and render template with Flask

--- Capstone_Blog/main.py
+++ Capstone_Blog/main.py
@@ -11,7 +11,7 @@ blog_data = response.json()
 # render homepage when server is started
 @app.route('/')
 def home():
-  return render_template("index.html", blog_data=blog_data)
+  return render_template("./index.html", blog_data=blog_data)
 
 # get_blog_post(x) is called as an href (when clicked) on the html doc,
 # when the <a href=""> is generated, it passes through the id of the correct blog post
@@ -24,7 +24,7 @@ def get_blog_post(post_num):
       blog_post = blog
     # print(blog_post, blog)
     # print(type(blog["id"]), type(post_num))
-  return render_template("post.html", blog=blog_post)
+  return render_template("./post.html", blog=blog_post)
 
 if __name__ == "__main__":
   app.run(debug=True)
