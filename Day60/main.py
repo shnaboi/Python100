31936d9045be22e6497cfbf20ac850827badc0df
Shane Karner <shane.karner@gmail.com>
2024-04-24 12:16:59 -0400

    Complete Day60 form lesson

--- Day60/main.py
+++ Day60/main.py
@@ -8,9 +8,10 @@ def home():
   return render_template("./index.html")
 
 @app.route('/login', methods=["POST"])
-def receive_data():
+def receive_form_data():
   username = request.form['username']
   password = request.form['password']
+  # can return render_template() after login is successful
   return f"{username} - {password}"
 
 if __name__ == "__main__":
